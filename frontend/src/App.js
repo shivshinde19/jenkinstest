c39aae637a587ba630f5c161c6c2d63dfca0d6c7
Shivvardhan Prashant Shinde <spshinde1919@gmail.com>
2026-01-07 12:45:06 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,47 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+  const callBackend = async () => {
+    try {
+      const response = await fetch("/api/hello");
+      const data = await response.text();
+      alert(data);
+    } catch (error) {
+      alert("Backend not reachable");
+    }
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+
+        <p>
+          React + Jenkins + .NET CI/CD 🚀
+        </p>
+
+        <button
+          onClick={callBackend}
+          style={{ padding: "10px 20px", fontSize: "16px" }}
+        >
+          Call Backend API
+        </button>
+
+        <br /><br />
+
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
