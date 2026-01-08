9c02af25fc71d8dfbd9e3851e180525d6b2ddae2
Shivvardhan Prashant Shinde <spshinde1919@gmail.com>
2026-01-08 10:13:34 +0530

    Remove emoji from CI/CD message

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
 
         <p>
-          React + Jenkins + .NET CI/CD 🚀
+          React + Jenkins + .NET CI/CD 
         </p>
 
         <button
